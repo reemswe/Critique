579fc22b512ad90b98dbc2e329ce36c42d820d19
reemswe <98765331+reemswe@users.noreply.github.com>
2022-04-25 17:30:51 +0300

    test

---
+++ app/src/main/java/com/example/critique/RetailerNotifaction.java
@@ -0,0 +1,14 @@
+package com.example.critique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetailerNotifaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer_notifaction);
+    }
+}
