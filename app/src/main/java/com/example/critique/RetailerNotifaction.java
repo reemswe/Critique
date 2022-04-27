f5c079ebe8c221f8637c447820673e9268524104
reemswe <98765331+reemswe@users.noreply.github.com>
2022-04-27 15:49:33 +0300

    test

--- app/src/main/java/com/example/critique/RetailerNotifaction.java
+++ app/src/main/java/com/example/critique/RetailerNotifaction.java
@@ -1,14 +1,63 @@
 package com.example.critique;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Random;
 
 public class RetailerNotifaction extends AppCompatActivity {
+
+    private EditText title, text;
+    private NotificationManagerCompat notificationManager;
+    private NotificationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_notifaction);
+        title = findViewById(R.id.notificationTitle);
+        text = findViewById(R.id.notificationText);
+        Button sendNotification = findViewById(R.id.sendNotification);
+
+        notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            manager = getSystemService(NotificationManager.class);
+        }
+
+        sendNotification.setOnClickListener(view -> {
+            String Title = title.getText().toString();
+            String Text = text.getText().toString();
+            sendNotification(Title, Text);
+        });
     }
+
+    public void sendNotification(String title, String text)
+    {   final String channelID = "critique channel";
+        Intent intent = new Intent(this, NotificationView.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        String value = "\n\n\n"+"The title of the notification: "+ title + "\n\n\n"+"The message of the notification: " + text + "\n\n\n";
+        intent.putExtra("msg",value);
+        intent.setAction(Long.toString(System.currentTimeMillis()));
+        PendingIntent pendingIntent;
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        builder.setContentIntent(pendingIntent);
+        notificationManager.notify(new Random().nextInt(), builder.build());
+    }
+
 }
--- app/src/main/java/com/example/critique/SignIn.java
+++ app/src/main/java/com/example/critique/SignIn.java
@@ -1,8 +1,12 @@
 package com.example.critique;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +21,9 @@ public class SignIn extends AppCompatActivity {
     Button login;
     TextView registerLink;
 
+    private NotificationManagerCompat notificationManager;
+    private NotificationManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +36,10 @@ public class SignIn extends AppCompatActivity {
 
         db = DBHelper.getInstance(this);
 
+        notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            manager = getSystemService(NotificationManager.class);
+
         registerLink.setOnClickListener(view -> SignIn.this.startActivity(new Intent(SignIn.this, SignUp.class)));
 
         login.setOnClickListener(view -> {
@@ -39,11 +50,25 @@ public class SignIn extends AppCompatActivity {
                 Toast.makeText(SignIn.this, "Please fill all field", Toast.LENGTH_SHORT).show();
             else {
                 boolean valid = db.validateInput(userN, pass);
-                if (valid)
-                    SignIn.this.startActivity(new Intent(SignIn.this, SignUp.class));
+                if (valid) {
+                    SignIn.this.startActivity(new Intent(SignIn.this, RetailerNotifaction.class));
+                    createChannel();
+                }
                 else
                     Toast.makeText(SignIn.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void createChannel(){
+        final String channelID = "critique channel";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = manager.getNotificationChannel(channelID);
+            if (channel == null) {
+                channel = new NotificationChannel(channelID, "Critique Channel", NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("This is Critique Channel");
+                manager.createNotificationChannel(channel);
+            }
+        }
     }
 }
